9bc514be0f00ef1667a0431cff72498e58084121
Kevin Stern <kstern284@gmail.com>
2024-12-05 23:27:16 -0500

    Combined 2024 Day05 parts and streamlined

--- 2024/Day05.m
+++ 2024/Day05.m
@@ -3,54 +3,32 @@ clear;clc
 data = split(fileread("Day05ex.txt"),char([10 10]));
 data = split(fileread("Day05.txt"),char([10 10]));
 
-part1(data)
-part2(data)
+parts(data)
 
-function part1(data)
+function parts(data)
 rules = double(split(string(splitlines(data{1})),'|'));
-pages = unique(rules)';
-locs = zeros(1,max(pages));
-ruleRows = false(size(rules,1),numel(pages));
-for i = 1:numel(pages)
-    locs(pages(i)) = i;
-    ruleRows(:,i) = ismember(rules(:,1),pages(i)) | ismember(rules(:,2),pages(i));
-end
-
 updates = string(splitlines(data{2}));
-total = 0;
+
+part1 = 0;
+part2 = 0;
 for update = updates(:)'
     updateNums = double(split(update,','))';
-    indPages = locs(updateNums);
-    relRules = rules(sum(ruleRows(:,indPages),2)==2,:);
-    [~,Locb] = ismember(relRules,updateNums);
+    [~,Locb] = ismember(rules,updateNums);
+    Locb(Locb(:,1)==0,1) = -Inf;
+    Locb(Locb(:,2)==0,2) = Inf;
     if all(Locb(:,1)<Locb(:,2))
-        total = total + updateNums((numel(updateNums)+1)/2);
-    end
-end
-disp(total)
-end
-
-function part2(data)
-rules = double(split(string(splitlines(data{1})),'|'));
-pages = unique(rules)';
-locs = zeros(1,max(pages));
-ruleRows = false(size(rules,1),numel(pages));
-for i = 1:numel(pages)
-    locs(pages(i)) = i;
-    ruleRows(:,i) = ismember(rules(:,1),pages(i)) | ismember(rules(:,2),pages(i));
-end
-
-updates = string(splitlines(data{2}));
-total = 0;
-for update = updates(:)'
-    updateNums = double(split(update,','))';
-    indPages = locs(updateNums);
-    relRules = rules(sum(ruleRows(:,indPages),2)==2,:);
-    [~,Locb] = ismember(relRules,updateNums);
-    if ~all(Locb(:,1)<Locb(:,2))
+        part1 = part1 + updateNums((numel(updateNums)+1)/2);
+    else
+        relRules = rules(all(isfinite(Locb),2),:);
         [B,BG] = groupcounts(relRules(:,2));
-        total = total + BG(B==numel(B)/2);
+        part2 = part2 + BG(B==numel(B)/2);
+
+        % Faster, but not as elegant
+        % sorted = unique(relRules(:,1));
+        % N = histcounts(relRules(:,1),[sorted;inf]);
+        % part2 = part2 + sorted(N==(numel(sorted)/2));
     end
 end
-disp(total)
+disp(part1)
+disp(part2)
 end
