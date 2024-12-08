f722a95242be3e9c1f6a7e4c8a3ef311949ba6b0
Kevin Stern <kstern284@gmail.com>
2024-12-08 00:06:15 -0500

    Rewrote 2024 Day07 based on suggestion from u/Deathranger999

--- 2024/Day07.m
+++ 2024/Day07.m
@@ -1,39 +1,50 @@
-% clear;clc
+clear;clc
 
 data = readlines("Day07ex.txt");
 data = readlines("Day07.txt");
 
-parts(data)
+tic
+parts(data, false)
+toc
 
-function parts(data)
+tic
+parts(data, true)
+toc
+
+function parts(data, doConcat)
 total = 0;
 for row = 1:numel(data)
     nums = double(extract(data(row),digitsPattern))';
-    goal = nums(1);
-    running = nums(2);
-    nums = nums(3:end);
+    running = nums(1);
+    nums = nums(end:-1:2);
 
-    if recurse(goal, running, nums, @plus) || ...
-        recurse(goal, running, nums, @times) || recurse(goal, running, nums, @concat)
-        total = total + goal;
+    bool = peel(running, nums, "minus", doConcat) || peel(running, nums, "rdivide", doConcat);
+    if bool || (doConcat && peel(running, nums, "deconcat", doConcat))
+        total = total + running;
     end
 end
 fprintf('%d\n',total)
 end
 
-function [bool, running, nums] = recurse(goal, running, nums, op)
-% takes in a
-running = op(running, nums(1));
+function bool = peel(running, nums, op, doConcat)
+relNum = nums(1);
 nums = nums(2:end);
-if isempty(nums) || running > goal
-    bool = goal==running;
+pow10 = 10^floor(log10(relNum)+1);
+if op == "deconcat" && relNum == mod(running,pow10)
+    running = floor(running/pow10);
+elseif op == "rdivide" && ~mod(running,relNum)
+    running = running/relNum;
+elseif op == "minus" && running > relNum
+    running = running - relNum;
+else
+    bool = false;
+    return
+end
+if isscalar(nums)
+    bool = running==nums;
     return
 end
 
-bool = recurse(goal, running, nums, @plus) || ...
-    recurse(goal, running, nums, @times) || recurse(goal, running, nums, @concat);
-end
-
-function out = concat(A, B)
-out = A*10^floor(log10(B)+1) + B;
+bool = peel(running, nums, "minus", doConcat) || peel(running, nums, "rdivide", doConcat) || ...
+    (doConcat && peel(running, nums, "deconcat", doConcat));
 end
